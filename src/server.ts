d546f795c31beaed99b525c8996b9dc04780b855
Erik <erik.p.schmitt@gmail.com>
2023-12-18 21:02:03 +0100

    Fixed body parser.

--- src/server.ts
+++ src/server.ts
@@ -1,20 +1,28 @@
 import express, { Express } from 'express';
 import { configureCors } from './startup/cors';
 import base from './routes/base';
+import user from './routes/user';
+import auth from './routes/auth';
+import project from './routes/project';
 import addRateLimiter from './startup/limitRate';
 import * as dotenv from 'dotenv';
+dotenv.config();
 import logger from './utils/logger';
 import connectToDatabase from './startup/db';
-dotenv.config();
+import bodyParser from 'body-parser';
+
 const app: Express = express();
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
 
 // startup
 configureCors(app);
 addRateLimiter(app);
 connectToDatabase();
-
-app.use(express.json());
 app.use('/', base);
+app.use('/user', user);
+app.use('/auth', auth);
+app.use('/project', project);
 
 const port: number = process.env.PORT ? parseInt(process.env.PORT) : 3000;
 const server = app.listen(port, '0.0.0.0', () => {
